spryker/product-list-search

commit b6dbbfd588c3701f5aa661faa7a35615be5c870e

FRW-10957 Adjusted P&S logic for product modules to avoid racing. (#11776)
FRW-10957 Adjusted P&S logic for product modules to avoid racing.

src/Spryker/Zed/ProductListSearch/Dependency/Facade/ProductListSearchToProductPageSearchFacadeInterface.php
@@ -17,17 +17,16 @@ interface ProductListSearchToProductPageSearchFacadeInterface
     public function publish(array $productAbstractIds): void;
 
     /**
-     * @param array<int> $productAbstractIds
-     * @param array<string> $pageDataExpanderPluginNames
+     * @param array<int, int> $productAbstractIdTimestampMap
      *
      * @return void
      */
-    public function refresh(array $productAbstractIds, array $pageDataExpanderPluginNames = []): void;
+    public function publishProductConcretes(array $productAbstractIdTimestampMap): void;
 
     /**
-     * @param array<int> $productConcreteIds
+     * @param array<int, int> $productAbstractIdTimestampMap
      *
      * @return void
      */
-    public function publishProductConcretes(array $productConcreteIds): void;
+    public function publishWithTimestamp(array $productAbstractIdTimestampMap): void;
 }

src/Spryker/Shared/ProductListSearch/Transfer/product_list_search.transfer.xml
@@ -53,4 +53,17 @@
         <property name="limit" type="int"/>
     </transfer>
 
+    <transfer name="HydrateEventsRequest">
+        <property name="eventEntities" type="EventEntity[]" singular="eventEntity" description="Collection of event entities to be processed"/>
+        <property name="foreignKeyName" type="string" description="Name of the foreign key field used for grouping or identification"/>
+    </transfer>
+
+    <transfer name="HydrateEventsResponse">
+        <property name="idTimestampMap" type="array" associative="true" singular="id" description="Map of event entity IDs to their corresponding timestamps"/>
+        <property name="foreignKeyTimestampMap" type="array" associative="true" singular="foreignKey" description="Map of foreign key values to their corresponding timestamps"/>
+    </transfer>
+
+    <transfer name="EventEntity">
+    </transfer>
+
 </transfers>

src/Spryker/Zed/ProductListSearch/Communication/Plugin/Event/Listener/ProductListCategorySearchListener.php
@@ -7,16 +7,16 @@
 
 namespace Spryker\Zed\ProductListSearch\Communication\Plugin\Event\Listener;
 
-use Orm\Zed\Category\Persistence\Map\SpyCategoryTableMap;
-use Spryker\Shared\ProductListSearch\ProductListSearchConfig;
+use ArrayObject;
+use Generated\Shared\Transfer\HydrateEventsRequestTransfer;
 use Spryker\Zed\Event\Dependency\Plugin\EventBulkHandlerInterface;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
-use Spryker\Zed\ProductCategory\Dependency\ProductCategoryEvents;
 
 /**
  * @method \Spryker\Zed\ProductListSearch\Communication\ProductListSearchCommunicationFactory getFactory()
  * @method \Spryker\Zed\ProductListSearch\Business\ProductListSearchFacadeInterface getFacade()
  * @method \Spryker\Zed\ProductListSearch\ProductListSearchConfig getConfig()
+ * @method \Spryker\Zed\ProductListSearch\Persistence\ProductListSearchRepositoryInterface getRepository()
  */
 class ProductListCategorySearchListener extends AbstractPlugin implements EventBulkHandlerInterface
 {
@@ -32,30 +32,13 @@ class ProductListCategorySearchListener extends AbstractPlugin implements EventB
      */
     public function handleBulk(array $eventEntityTransfers, $eventName): void
     {
-        $categoryIds = $this->getFactory()->getEventBehaviorFacade()->getEventTransferIds($eventEntityTransfers);
-
-        $this->getFactory()->getProductPageSearchFacade()->refresh(
-            $this->getFacade()->getProductAbstractIdsByCategoryIds($categoryIds),
-            [ProductListSearchConfig::PLUGIN_PRODUCT_LIST_DATA],
+        $hydrateEventsResponseTransfer = $this->getFactory()->getEventBehaviorFacade()->hydrateEventDataTransfer(
+            (new HydrateEventsRequestTransfer())
+                ->setEventEntities(new ArrayObject($eventEntityTransfers)),
         );
-    }
 
-    /**
-     * @param array<\Generated\Shared\Transfer\EventEntityTransfer> $eventTransfers
-     * @param string $eventName
-     *
-     * @return array<int>
-     */
-    protected function getCategoryIds($eventTransfers, $eventName): array
-    {
-        if ($eventName === ProductCategoryEvents::ENTITY_SPY_PRODUCT_CATEGORY_CREATE) {
-            return $this->getFactory()
-                ->getEventBehaviorFacade()
-                ->getEventTransferIds($eventTransfers);
-        }
-
-        return $this->getFactory()
-            ->getEventBehaviorFacade()
-            ->getEventTransferForeignKeys($eventTransfers, SpyCategoryTableMap::COL_ID_CATEGORY);
+        $this->getFactory()->getProductPageSearchFacade()->publishWithTimestamp(
+            $this->getRepository()->getProductAbstractIdsTimestampMap($hydrateEventsResponseTransfer->getIdTimestampMap()),
+        );
     }
 }

src/Spryker/Zed/ProductListSearch/Communication/Plugin/Event/Listener/ProductListProductCategorySearchListener.php
@@ -7,8 +7,9 @@
 
 namespace Spryker\Zed\ProductListSearch\Communication\Plugin\Event\Listener;
 
+use ArrayObject;
+use Generated\Shared\Transfer\HydrateEventsRequestTransfer;
 use Orm\Zed\ProductList\Persistence\Map\SpyProductListCategoryTableMap;
-use Spryker\Shared\ProductListSearch\ProductListSearchConfig;
 use Spryker\Zed\Event\Dependency\Plugin\EventBulkHandlerInterface;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 
@@ -16,6 +17,7 @@ use Spryker\Zed\Kernel\Communication\AbstractPlugin;
  * @method \Spryker\Zed\ProductListSearch\Communication\ProductListSearchCommunicationFactory getFactory()
  * @method \Spryker\Zed\ProductListSearch\Business\ProductListSearchFacadeInterface getFacade()
  * @method \Spryker\Zed\ProductListSearch\ProductListSearchConfig getConfig()
+ * @method \Spryker\Zed\ProductListSearch\Persistence\ProductListSearchRepositoryInterface getRepository()
  */
 class ProductListProductCategorySearchListener extends AbstractPlugin implements EventBulkHandlerInterface
 {
@@ -31,13 +33,14 @@ class ProductListProductCategorySearchListener extends AbstractPlugin implements
      */
     public function handleBulk(array $eventEntityTransfers, $eventName): void
     {
-        $categoryIds = $this->getFactory()
-            ->getEventBehaviorFacade()
-            ->getEventTransferForeignKeys($eventEntityTransfers, SpyProductListCategoryTableMap::COL_FK_CATEGORY);
+        $hydrateEventsResponseTransfer = $this->getFactory()->getEventBehaviorFacade()->hydrateEventDataTransfer(
+            (new HydrateEventsRequestTransfer())
+                ->setEventEntities(new ArrayObject($eventEntityTransfers))
+                ->setForeignKeyName(SpyProductListCategoryTableMap::COL_FK_CATEGORY),
+        );
 
-        $this->getFactory()->getProductPageSearchFacade()->refresh(
-            $this->getFacade()->getProductAbstractIdsByCategoryIds($categoryIds),
-            [ProductListSearchConfig::PLUGIN_PRODUCT_LIST_DATA],
+        $this->getFactory()->getProductPageSearchFacade()->publishWithTimestamp(
+            $this->getRepository()->getProductAbstractIdsTimestampMap($hydrateEventsResponseTransfer->getForeignKeyTimestampMap()),
         );
     }
 }

src/Spryker/Zed/ProductListSearch/Communication/Plugin/Event/Listener/ProductListProductConcreteSearchListener.php
@@ -7,8 +7,9 @@
 
 namespace Spryker\Zed\ProductListSearch\Communication\Plugin\Event\Listener;
 
+use ArrayObject;
+use Generated\Shared\Transfer\HydrateEventsRequestTransfer;
 use Orm\Zed\ProductList\Persistence\Map\SpyProductListProductConcreteTableMap;
-use Spryker\Shared\ProductListSearch\ProductListSearchConfig;
 use Spryker\Zed\Event\Dependency\Plugin\EventBulkHandlerInterface;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 
@@ -16,6 +17,7 @@ use Spryker\Zed\Kernel\Communication\AbstractPlugin;
  * @method \Spryker\Zed\ProductListSearch\Communication\ProductListSearchCommunicationFactory getFactory()
  * @method \Spryker\Zed\ProductListSearch\Business\ProductListSearchFacadeInterface getFacade()
  * @method \Spryker\Zed\ProductListSearch\ProductListSearchConfig getConfig()
+ * @method \Spryker\Zed\ProductListSearch\Persistence\ProductListSearchRepositoryInterface getRepository()
  */
 class ProductListProductConcreteSearchListener extends AbstractPlugin implements EventBulkHandlerInterface
 {
@@ -31,13 +33,14 @@ class ProductListProductConcreteSearchListener extends AbstractPlugin implements
      */
     public function handleBulk(array $eventEntityTransfers, $eventName): void
     {
-        $concreteIds = $this->getFactory()
-            ->getEventBehaviorFacade()
-            ->getEventTransferForeignKeys($eventEntityTransfers, SpyProductListProductConcreteTableMap::COL_FK_PRODUCT);
+        $hydrateEventsResponseTransfer = $this->getFactory()->getEventBehaviorFacade()->hydrateEventDataTransfer(
+            (new HydrateEventsRequestTransfer())
+                ->setEventEntities(new ArrayObject($eventEntityTransfers))
+                ->setForeignKeyName(SpyProductListProductConcreteTableMap::COL_FK_PRODUCT),
+        );
 
-        $this->getFactory()->getProductPageSearchFacade()->refresh(
-            $this->getFacade()->getProductAbstractIdsByConcreteIds($concreteIds),
-            [ProductListSearchConfig::PLUGIN_PRODUCT_LIST_DATA],
+        $this->getFactory()->getProductPageSearchFacade()->publishWithTimestamp(
+            $this->getRepository()->getProductAbstractIdsTimestampMapByConcreteIds($hydrateEventsResponseTransfer->getForeignKeyTimestampMap()),
         );
     }
 }

src/Spryker/Zed/ProductListSearch/Communication/Plugin/Event/Listener/ProductListSearchListener.php
@@ -7,7 +7,8 @@
 
 namespace Spryker\Zed\ProductListSearch\Communication\Plugin\Event\Listener;
 
-use Spryker\Shared\ProductListSearch\ProductListSearchConfig;
+use ArrayObject;
+use Generated\Shared\Transfer\HydrateEventsRequestTransfer;
 use Spryker\Zed\Event\Dependency\Plugin\EventBulkHandlerInterface;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 
@@ -30,12 +31,23 @@ class ProductListSearchListener extends AbstractPlugin implements EventBulkHandl
      */
     public function handleBulk(array $eventEntityTransfers, $eventName)
     {
-        $productListIds = $this->getFactory()
-            ->getEventBehaviorFacade()->getEventTransferIds($eventEntityTransfers);
+        $hydrateEventsResponseTransfer = $this->getFactory()->getEventBehaviorFacade()->hydrateEventDataTransfer(
+            (new HydrateEventsRequestTransfer())
+                ->setEventEntities(new ArrayObject($eventEntityTransfers)),
+        );
+        $productListIdsTimestampMap = $hydrateEventsResponseTransfer->getIdTimestampMap();
 
-        $this->getFactory()->getProductPageSearchFacade()->refresh(
-            $this->getFactory()->getProductListFacade()->getProductAbstractIdsByProductListIds($productListIds),
-            [ProductListSearchConfig::PLUGIN_PRODUCT_LIST_DATA],
+        if (!$productListIdsTimestampMap) {
+            return;
+        }
+
+        $this->getFactory()->getProductPageSearchFacade()->publish(
+            array_fill_keys(
+                $this->getFactory()->getProductListFacade()->getProductAbstractIdsByProductListIds(
+                    $hydrateEventsResponseTransfer->getIdTimestampMap(),
+                ),
+                min($productListIdsTimestampMap),
+            ),
         );
     }
 }

src/Spryker/Zed/ProductListSearch/Communication/Plugin/Event/Listener/ProductSearchListener.php
@@ -7,8 +7,9 @@
 
 namespace Spryker\Zed\ProductListSearch\Communication\Plugin\Event\Listener;
 
+use ArrayObject;
+use Generated\Shared\Transfer\HydrateEventsRequestTransfer;
 use Orm\Zed\Product\Persistence\Map\SpyProductTableMap;
-use Spryker\Shared\ProductListSearch\ProductListSearchConfig;
 use Spryker\Zed\Event\Dependency\Plugin\EventBulkHandlerInterface;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 
@@ -31,10 +32,12 @@ class ProductSearchListener extends AbstractPlugin implements EventBulkHandlerIn
      */
     public function handleBulk(array $eventEntityTransfers, $eventName): void
     {
-        $productAbstractIds = $this->getFactory()
-            ->getEventBehaviorFacade()
-            ->getEventTransferForeignKeys($eventEntityTransfers, SpyProductTableMap::COL_FK_PRODUCT_ABSTRACT);
+        $hydrateEventsResponseTransfer = $this->getFactory()->getEventBehaviorFacade()->hydrateEventDataTransfer(
+            (new HydrateEventsRequestTransfer())
+                ->setEventEntities(new ArrayObject($eventEntityTransfers))
+                ->setForeignKeyName(SpyProductTableMap::COL_FK_PRODUCT_ABSTRACT),
+        );
 
-        $this->getFactory()->getProductPageSearchFacade()->refresh($productAbstractIds, [ProductListSearchConfig::PLUGIN_PRODUCT_LIST_DATA]);
+        $this->getFactory()->getProductPageSearchFacade()->publishWithTimestamp($hydrateEventsResponseTransfer->getForeignKeyTimestampMap());
     }
 }

src/Spryker/Zed/ProductListSearch/Dependency/Facade/ProductListSearchToEventBehaviorFacadeBridge.php
@@ -7,6 +7,9 @@
 
 namespace Spryker\Zed\ProductListSearch\Dependency\Facade;
 
+use Generated\Shared\Transfer\HydrateEventsRequestTransfer;
+use Generated\Shared\Transfer\HydrateEventsResponseTransfer;
+
 class ProductListSearchToEventBehaviorFacadeBridge implements ProductListSearchToEventBehaviorFacadeInterface
 {
     /**
@@ -41,5 +44,15 @@ class ProductListSearchToEventBehaviorFacadeBridge implements ProductListSearchT
     public function getEventTransferIds(array $eventTransfers): array
     {
         return $this->eventBehaviorFacade->getEventTransferIds($eventTransfers);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\HydrateEventsRequestTransfer $hydrateEventsRequestTransfer
+     *
+     * @return \Generated\Shared\Transfer\HydrateEventsResponseTransfer
+     */
+    public function hydrateEventDataTransfer(HydrateEventsRequestTransfer $hydrateEventsRequestTransfer): HydrateEventsResponseTransfer
+    {
+        return $this->eventBehaviorFacade->hydrateEventDataTransfer($hydrateEventsRequestTransfer);
     }
 }

commit 91ce4eccc36c8e1ccb90010f1bf17f311dfbc04b

Update ProductListSearchToProductPageSearchFacadeInterface.php
Fixes: Regression introduced by: b6dbbfd588c3701f5aa661faa7a35615be5c870e
Bug: publishProductConcretes() now mixes up abstract IDs/timestamps with concrete IDs. This breaks syncing for ProductListEvents::ENTITY_SPY_PRODUCT_LIST_PRODUCT_CONCRETE_* events.
Fix: Restore the adapter/interface contract to accept concrete IDs and forward them unchanged to ProductPageSearchFacadeInterface::publishProductConcretes().

src/Spryker/Zed/ProductListSearch/Dependency/Facade/ProductListSearchToProductPageSearchFacadeInterface.php
@@ -17,11 +17,11 @@ interface ProductListSearchToProductPageSearchFacadeInterface
     public function publish(array $productAbstractIds): void;
 
     /**
-     * @param array<int, int> $productAbstractIdTimestampMap
+     * @param array<int> $productConcreteIds
      *
      * @return void
      */
-    public function publishProductConcretes(array $productAbstractIdTimestampMap): void;
+    public function publishProductConcretes(array $productConcreteIds): void;
 
     /**
      * @param array<int, int> $productAbstractIdTimestampMap